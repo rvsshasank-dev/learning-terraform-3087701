f8c1a9c967af011870a71fd7f1b5b8c254d5a1be
rvsshasank-dev <rvsshasank@gmail.com>
2025-03-21 14:58:07 -0400

    vpc update 2

--- main.tf
+++ main.tf
@@ -24,7 +24,6 @@ module "blog_vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -34,6 +33,8 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
