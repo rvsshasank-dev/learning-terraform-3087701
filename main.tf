91f5c468718b1d85ac44629e2c08264aca3aeeb3
rvsshasank-dev <rvsshasank@gmail.com>
2025-04-10 18:07:59 -0400

    alb-8

--- main.tf
+++ main.tf
@@ -67,17 +67,18 @@ module "alb" {
   }
 
   target_groups = {
+    instance = {
       name_prefix      = "blog"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
-      }
+      target_id = aws_instance.blog.id
+          
+        
+      
+    }  
    }
+
   
 
 
