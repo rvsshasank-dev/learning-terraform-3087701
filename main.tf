84f68449f5d7429797724df1a70dfa2219650450
rvsshasank-dev <rvsshasank@gmail.com>
2025-04-11 10:22:13 -0400

    commit 4

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "my_vpc"{
 }
 
 resource "aws_subnet" "public_subnet" {
-    vpc_id = aws_vpc.my-vpc.id
+    vpc_id = aws_vpc.my_vpc.id
     cidr_block = "10.0.0.0/25"
     map_public_ip_on_launch = true
     availability_zone =  "us-west-2b"  
