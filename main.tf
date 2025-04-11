84118a56844c614de8186a86146eef071f4a3b30
rvsshasank-dev <rvsshasank@gmail.com>
2025-04-11 10:28:35 -0400

    Update main.tf
    commit 5

--- main.tf
+++ main.tf
@@ -8,3 +8,19 @@ resource "aws_subnet" "public_subnet" {
     map_public_ip_on_launch = true
     availability_zone =  "us-west-2b"  
 }
+
+resource "aws_internet_gateway" "igw"{
+    vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "my_route"{
+    vpc_id = aws_vpc.my_vpc.id
+    
+}
+
+resource "aws_route" "public-route-table"{
+    route_table_id =aws_route_table.my_route.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+
+}
